9da51d0cba9f62e98caf75a31e58a574602760d3
i-play-lf2 <914637118@qq.com>
2025-06-18 15:13:26 +0000

    wip

--- projects/mods/roll-protagonist/RollProtagonist.Frontend/OnStartNewGame.cs
+++ projects/mods/roll-protagonist/RollProtagonist.Frontend/OnStartNewGame.cs
@@ -2,42 +2,39 @@ using HarmonyLib;
 using UnityEngine;
 using GameData.Utilities;
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace RollProtagonist.Frontend;
 
 [HarmonyPatch(typeof(UI_NewGame))]
 internal static class OnStartNewGamePatcher
 {
+    [HarmonyPatch("DoStartNewGame"), HarmonyReversePatch]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void DoStartNewGameOrigin(UI_NewGame uiNewGame)
+    {
+        throw null!;
+    }
+
     [HarmonyPatch("DoStartNewGame"), HarmonyPrefix]
     private static bool DoStartNewGamePrefix(UI_NewGame __instance)
     {
-        InDoStartGame = true;
-
         __instance.StartCoroutine(DoStartNewGame(__instance));
 
         return false;
     }
 
-
-    private static IEnumerator DoStartNewGame(UI_NewGame newGameInstance)
+    private static IEnumerator DoStartNewGame(UI_NewGame uiNewGame)
     {
 
         AdaptableLog.Info("before DoStartNewGame");
 
-        Traverse.Create(newGameInstance).Method("DoStartNewGame").GetValue();
+        DoStartNewGameOrigin(uiNewGame);
 
         AdaptableLog.Info("after DoStartNewGame");
 
         yield return new WaitForSeconds(3);
 
-        InDoStartGame = false;
+        AdaptableLog.Info("after WaitForSeconds");
     }
-
-    [HarmonyPatch("OnStartNewGame"), HarmonyPrefix]
-    private static bool OnStartNewGame()
-    {
-        return !InDoStartGame;
-    }
-
-    private static bool InDoStartGame = false;
 }
